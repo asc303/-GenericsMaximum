a5a5185f080b98905d4f6f91a7d4724f152e8087
asc303 <chauhansam94@gmail.com>
2021-11-19 23:05:31 +0530

    [ashutosh] add:Maximum Integer

--- GenericsMaximum/GenericsMaximum/MaximumComputation.cs
+++ GenericsMaximum/GenericsMaximum/MaximumComputation.cs
@@ -21,35 +21,5 @@ namespace GenericsMaximum
                 return thirdNumber;
             }
         }
-        public static double FindMax(double firstNumber, double secondNumber, double thirdNumber)
-        {
-            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
-            {
-                return firstNumber;
-            }
-            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
-            {
-                return secondNumber;
-            }
-            else
-            {
-                return thirdNumber;
-            }
-        }
-        public static string FindMax(string firstWord, string secondWord, string thirdWord)
-        {
-            if (firstWord.CompareTo(secondWord) > 0 && firstWord.CompareTo(thirdWord) > 0)
-            {
-                return firstWord;
-            }
-            if (secondWord.CompareTo > (firstWord) > 0 && secondWord.CompareTo(thirdWord) > 0)
-            {
-                return secondWord;
-            }
-            else
-            {
-                return thirdWord;
-            }
-        }
     }
 }
--- GenericsMaximum/GenericsMaximum/Program.cs
+++ GenericsMaximum/GenericsMaximum/Program.cs
@@ -10,12 +10,12 @@ namespace GenericsMaximum
             while (flag)
             {
                 Console.WriteLine("Welcome To Generic Maximum Problems");
-                Console.WriteLine("Enter the Program that want to be executed : \n 1. Maximum Integer \n 2. Maximum Float \n 3. Maximum String \n 4. Exit");
+                Console.WriteLine("Enter the Program that want to be executed : \n 1. Maximum Integer \n 2.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        Console.WriteLine("Enter First number, Second number, Third number to check : ");
+                        Console.WriteLine("Enter First number, Second number, Third number : ");
                         int first = Convert.ToInt32(Console.ReadLine());
                         int second = Convert.ToInt32(Console.ReadLine());
                         int third = Convert.ToInt32(Console.ReadLine());
@@ -23,22 +23,6 @@ namespace GenericsMaximum
                         Console.WriteLine("The Maximum Number is : " + result);
                         break;
                     case 2:
-                        Console.WriteLine("Enter First number, Second number, Third number to check : ");
-                        double firstNum = Convert.ToDouble(Console.ReadLine());
-                        double secondNum = Convert.ToDouble(Console.ReadLine());
-                        double thirdNum = Convert.ToDouble(Console.ReadLine());
-                        double res = MaximumComputation.FindMax(firstNum, secondNum, thirdNum);
-                        Console.WriteLine("The Maximum Number is : " + res);
-                        break;
-                    case 3:
-                        Console.WriteLine("Enter Three Strings to check : ");
-                        string firstName = Convert.ToString(Console.ReadLine());
-                        string secondName = Convert.ToString(Console.ReadLine());
-                        string thirdName = Convert.ToString(Console.ReadLine());
-                        string largest = MaximumComputation.FindMax(firstName, secondName, thirdName);
-                        Console.WriteLine("The Largest String is : " + largest);
-                        break;
-                    case 4:
                         flag = false;
                         break;
                 }
